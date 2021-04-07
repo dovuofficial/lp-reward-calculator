b4ae5b893904b4f12fac3354a8e0c029988cacfa
Matt Smithies <flyinggazelle@hotmail.co.uk>
2021-04-07 08:31:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var connection = mysql.createConnection({
 connection.connect();
 
 //CHANGE THIS: Reward devided by the number of hours and minutes in the period.
-var reward = 500000 / 744 / 60;
+var reward = 3000000 / 744 / 60;
 
 //CHANGE THESE DATES:
 var date = new Date('Mar 1 2021 00:00:00 GMT+0000 (UTC)').getTime();
@@ -19,7 +19,7 @@ while(date < new Date('Apr 1 2021 00:00:00 GMT+0000 (UTC)')) {
     date += (60 * 1000);
 
     var query = connection.query({
-        sql: "insert into awards(address, amount) select trans.from, sum(quantity) / (select sum(qty) from (select trans.from, sum(quantity) qty from trans where UnixTimestamp < ? group by trans.from having sum(quantity) > 0) daily_total) * ? award from trans where UnixTimestamp < ? group by trans.from having sum(quantity) > 0", 
+        sql: "insert into awards(address, amount) select trans.from, sum(quantity) / (select sum(qty) from (select trans.from, sum(quantity) qty from trans where UnixTimestamp < ? group by trans.from having sum(quantity) > 0) daily_total) * ? award from trans where UnixTimestamp < ? group by trans.from having sum(quantity) > 0",
         values: [date.valueOf() / 1000, reward, date.valueOf() / 1000]
     },
         function (error, results, fields) {
